fa30ab1396611e3a7832800eb984b8de433449a9
Harsh Mehta <98632476+aka-Harsh@users.noreply.github.com>
2024-08-24 01:35:58 +0530

    Update lstm_model.py

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -6,15 +6,14 @@ from tensorflow.keras.optimizers import Adam
 class LSTMModel:
     def __init__(self):
         self.model = Sequential([
-            LSTM(50, activation='relu', input_shape=(1, 5)),
+            LSTM(50, activation='relu', input_shape=(60, 5), return_sequences=True),
+            LSTM(50, activation='relu'),
             Dense(1)
         ])
         self.model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
 
     def fit(self, X_train, y_train):
-        X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
         self.model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
 
     def predict(self, X_test):
-        X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
-        return self.model.predict(X_test).flatten()
+        return self.model.predict(X_test)
