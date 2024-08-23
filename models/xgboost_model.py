c5b2880f34f02c0873df8a347f06fa3aa96ba1ea
Harsh Mehta <98632476+aka-Harsh@users.noreply.github.com>
2024-08-24 02:02:19 +0530

    Update xgboost_model.py

--- models/xgboost_model.py
+++ models/xgboost_model.py
@@ -11,4 +11,11 @@ class XGBoostModel:
 
     def predict(self, X_test):
         X_test_2d = X_test.reshape(X_test.shape[0], -1)
-        return self.model.predict(X_test_2d).reshape(-1, 1)
+        predictions = []
+        for i in range(X_test_2d.shape[0]):
+            pred = self.model.predict(X_test_2d[i:i+1])
+            predictions.append(pred[0])
+            if i < X_test_2d.shape[0] - 1:
+                X_test_2d[i+1, :-5] = X_test_2d[i, 5:]
+                X_test_2d[i+1, -5:] = pred
+        return np.array(predictions).reshape(-1, 1)
